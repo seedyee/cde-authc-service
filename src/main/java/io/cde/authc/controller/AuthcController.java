7c69feaea239006f6b8deebb6ed49ff755b09d9e
Fangcai Liao <liaofangcai@Fangcais-MacBook-Air.local>
2016-11-15 00:35:59 -0800

    修改接口返回数据格式

--- src/main/java/io/cde/authc/controller/AuthcController.java
+++ src/main/java/io/cde/authc/controller/AuthcController.java
@@ -1,18 +1,18 @@
 package io.cde.authc.controller;
 
-import java.util.Collection;
-
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
-import org.apache.shiro.session.Session;
 import org.apache.shiro.subject.Subject;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.cde.authc.domaim.Error;
+import io.cde.authc.domaim.Model;
+import io.cde.authc.tools.ResultUtils;
 
 /**
 * @author 作者 Fangcai Liao
@@ -27,35 +27,27 @@ public class AuthcController {
    * 用户登录.
    */
   @RequestMapping(value = "/signin", method = RequestMethod.POST)
-  public String signin(@RequestParam String principal, @RequestParam String password) {
+  public Object signin(@RequestParam String principal, @RequestParam String password) {
     Subject subject = SecurityUtils.getSubject();
-    Session session = subject.getSession();
-    Collection collection =  session.getAttributeKeys();
-    System.out.println(subject.isAuthenticated()+"**"+subject.isRemembered());
-    for (Object key: collection) {
-      System.out.println("key = "+key+"****"+"value = "+session.getAttribute(key));
-    }
-    subject.isRemembered();
-    subject.isAuthenticated();
     UsernamePasswordToken token = new UsernamePasswordToken(principal,password);
     try{
       subject.login(token);
-      return "signin success";
+      return null;
     }catch (IncorrectCredentialsException e){
       e.printStackTrace();
-      return "密码和用户不匹配";
+      return ResultUtils.resultError(1000004, "密码和用户不匹配");
     }catch (UnknownAccountException e) {
       e.printStackTrace();
-      return "该用户不存在";
+      return ResultUtils.resultError(1000005, "用户不存在");
     }
   }
   /**
    * 用户登出.
    */
   @RequestMapping(value = "/signout", method = RequestMethod.POST)
-  public String signout() {
+  public Object signousignoutt() {
     Subject subject = SecurityUtils.getSubject();
     subject.logout();
-    return "signout success";
+    return null;
   }
 }
