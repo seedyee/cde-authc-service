94af27753c44de46d05e85ec822a1d248bf037ca
Fangcai Liao <liaofangcai@Fangcais-MacBook-Air.local>
2016-11-16 16:45:51 -0800

    修改接口返回数据的组装逻辑

--- src/main/java/io/cde/authc/controller/AuthcController.java
+++ src/main/java/io/cde/authc/controller/AuthcController.java
@@ -3,15 +3,14 @@ package io.cde.authc.controller;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.UnknownAccountException;
-import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import io.cde.authc.domaim.Error;
-import io.cde.authc.domaim.Model;
 import io.cde.authc.tools.ResultUtils;
+
+import javax.servlet.ServletRequest;
+
 
 /**
 * @author 作者 Fangcai Liao
@@ -26,22 +25,17 @@ public class AuthcController {
    * 用户登录.
    */
   @RequestMapping(value = "/signin", method = RequestMethod.POST)
-  public Object signin(@RequestParam String principal, @RequestParam String password) {
-    Subject subject = SecurityUtils.getSubject();
-    UsernamePasswordToken token = new UsernamePasswordToken(principal,password);
-    try{
-      subject.login(token);
-      return null;
-    }catch (IncorrectCredentialsException e){
-      e.printStackTrace();
-      return ResultUtils.resultError(1000004, "密码和用户不匹配");
-    }catch (UnknownAccountException e) {
-      e.printStackTrace();
+  public Object signin(ServletRequest req) {
+    String exceptionClassName = (String)req.getAttribute("shiroLoginFailure");
+
+    if(UnknownAccountException.class.getName().equals(exceptionClassName)) {
       return ResultUtils.resultError(1000005, "用户不存在");
-    }catch (Exception e) {
-      e.printStackTrace();
+    } else if(IncorrectCredentialsException.class.getName().equals(exceptionClassName)) {
+      return ResultUtils.resultError(1000004, "密码和用户不匹配");
+    } else if(exceptionClassName != null) {
       return ResultUtils.resultError(1000005, "系统错误");
     }
+    return null;
   }
   /**
    * 用户登出.
