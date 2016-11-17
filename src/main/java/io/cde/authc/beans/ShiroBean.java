163e4895724e34a74b99ba5ede6c02872cdb80ee
Fangcai Liao <liaofangcai@Fangcais-MacBook-Air.local>
2016-11-16 16:44:27 -0800

    添加 rememberMe 功能

--- src/main/java/io/cde/authc/beans/ShiroBean.java
+++ src/main/java/io/cde/authc/beans/ShiroBean.java
@@ -1,17 +1,26 @@
 package io.cde.authc.beans;
 
+import io.cde.authc.filter.AuthcFilter;
+import org.apache.shiro.session.mgt.SessionManager;
 import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.util.ByteSource;
+import org.apache.shiro.web.mgt.CookieRememberMeManager;
 import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
 import org.apache.shiro.web.mgt.WebSecurityManager;
 import org.apache.shiro.web.servlet.SimpleCookie;
 import org.apache.shiro.web.session.mgt.DefaultWebSessionManager;
 import org.apache.shiro.web.session.mgt.WebSessionManager;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.cde.authc.realm.AuthcRealm;
 import io.cde.authc.redis.RedisDAO;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.Filter;
 
 /**
 * @author 作者 Fangcai Liao
@@ -21,8 +30,30 @@ import io.cde.authc.redis.RedisDAO;
 @Configuration
 public class ShiroBean {
 
+  @Value("${SimpleCookie.name}")
+  private String simpleCookieName;
+
+  @Value("${SimpleCookie.HttpOnly}")
+  private boolean simpleCookieHttpOnly;
+
+  @Value("${SimpleCookie.MaxAge}")
+  private int simpleCookieMaxAge;
+
+  @Value("${RememberMeCookie.name}")
+  private String rememberMeCookieName;
+
+  @Value("${RememberMeCookie.HttpOnly}")
+  private boolean rememberMeCookieHttpOnly;
+
+  @Value("${RememberMeCookie.MaxAge}")
+  private int rememberMeCookieMaxAge;
+
+  private static Map<String, String> filterChainDefinitionMap = new HashMap<>();
+
+  private static Map<String, Filter> filterHashMap = new HashMap<>();
+
   @Bean
-  public WebSessionManager getWebSessionManager(){
+  public SessionManager getWebSessionManager(){
     DefaultWebSessionManager defaultWebSessionManager = new DefaultWebSessionManager();
     defaultWebSessionManager.setSessionDAO(getRedisSessionDAO());
     defaultWebSessionManager.setSessionIdCookie(getSimpleCookie());
@@ -44,6 +75,7 @@ public class ShiroBean {
     securityManager.setRealm(getAuthcRealm());
     securityManager.setSessionManager(getWebSessionManager());
 
+    securityManager.setRememberMeManager(getCookieRememberMeManager());
     return securityManager;
   }
 
@@ -51,6 +83,11 @@ public class ShiroBean {
   public ShiroFilterFactoryBean getShiroFilterFactoryBean() {
     ShiroFilterFactoryBean shiroFilterFactoryBean = new ShiroFilterFactoryBean();
     shiroFilterFactoryBean.setSecurityManager(getWebSecurityManager());
+    filterChainDefinitionMap.put("/authc/signin", "authc");
+    shiroFilterFactoryBean
+            .setFilterChainDefinitionMap(filterChainDefinitionMap);
+    filterHashMap.put("authc", getFilter());
+    shiroFilterFactoryBean.setFilters(filterHashMap);
 
     return shiroFilterFactoryBean;
   }
@@ -58,14 +95,38 @@ public class ShiroBean {
   @Bean
   public AuthcRealm getAuthcRealm(){
     AuthcRealm authcRealm = new AuthcRealm();
+    authcRealm.setCachingEnabled(true);
 
     return authcRealm;
   }
 
+  @Bean
+  public CookieRememberMeManager getCookieRememberMeManager() {
+    CookieRememberMeManager cookieRememberMeManager= new CookieRememberMeManager();
+    cookieRememberMeManager.setCookie(getRememberMeCookie());
+
+    return  cookieRememberMeManager;
+  }
+
+  @Bean
+  public Filter getFilter() {
+    AuthcFilter authcFilter = new AuthcFilter();
+
+    return authcFilter;
+  }
+
+  public SimpleCookie getRememberMeCookie() {
+    SimpleCookie simpleCookie = new SimpleCookie(rememberMeCookieName);
+    simpleCookie.setHttpOnly(rememberMeCookieHttpOnly);
+    simpleCookie.setMaxAge(rememberMeCookieMaxAge);
+
+    return simpleCookie;
+  }
+
   public SimpleCookie getSimpleCookie() {
-    SimpleCookie simpleCookie = new SimpleCookie("io.cde.auth.session.id");
-    simpleCookie.setHttpOnly(true);
-    simpleCookie.setMaxAge(-1);
+    SimpleCookie simpleCookie = new SimpleCookie(simpleCookieName);
+    simpleCookie.setHttpOnly(simpleCookieHttpOnly);
+    simpleCookie.setMaxAge(simpleCookieMaxAge);
 
     return simpleCookie;
   }
